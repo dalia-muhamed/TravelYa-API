5aa758b81fdd98b1f38dd4c475f673b6b8dad1a2
Kerollos Fessal <kerofessal77@gmail.com>
2023-10-27 22:39:34 +0300

    kero update

--- data.js
+++ data.js
@@ -90,22 +90,32 @@ export function getRestaurant() {
       name: "Lucille's Restaurant Cairo",
       image:
         'https://lh5.googleusercontent.com/p/AF1QipM6j2cjs5U7fK_EiQurPmnX0uqiPUNnUST4rS06=w260-h175-n-k-no',
-      id: 11,
+      location: '54 Rd 9, Maadi, Cairo Governorate 4212011',
+        id: 11,
     },
     {
       name: 'Tree Lounge Maadi ',
       image:
         'https://lh3.googleusercontent.com/p/AF1QipMePG6E4vzRKTRkjDGZdsTZytbKK7YCnSILoAV4=s1360-w1360-h1020',
-      id: 11,
+      location:'Maadi Al Khabiri Ash Sharqeyah, Maadi, Cairo Governorate 11728',
+        id: 11,
     },
     {
-      name: 'Greece',
-      image:
-        'https://lh3.googleusercontent.com/p/AF1QipNW3sTgyfBm62ql8vj1JG76RC1XjjN2b0bS2Yuq=s1360-w1360-h1020',
+      name: 'Griix',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipMdCIJ14s2gA4Jf9zGzp7nsQFyQnX2JaxzixV-E=s680-w680-h510',
+      location: '38 street 231, degla, Maadi, Cairo Governorate',
       id: 11,
     },
     {
       name: 'Lucca Italian Restaurant',
+      image: 'https://storage.kempinski.com/cdn-cgi/image/w=480,h=550,f=auto,g=auto,fit=cover/ki-cms-prod/images/1/6/2/5/95261-1-eng-GB/62213d1650f3-73658083_4K.jpg',
+      location: 'Ring Rd, Second New Cairo, Cairo, Cairo Governorate 11477',
+      id: 11,
+    },
+    {
+      name: 'The Grill',
+      image: 'https://resizer.otstatic.com/v2/photos/xlarge/4/26670259.webp',
+      location: 'Semiramis InterContinental, Cornich, El Nile, Cairo Governorate 11511',
       id: 11,
     },
     {
@@ -113,6 +123,61 @@ export function getRestaurant() {
       image:
         'https://www.australia.com/content/australia/en/things-to-do/food-and-drinks/best-restaurants-in-australia/jcr:content/mainParsys/imagewithcaption_1431434042/LargeImageTile/largeImageSrc.adapt.740.medium.jpg',
       id: 44,
+    },
+  ];
+}
+
+export function getThingsToDO() {
+  return [
+    {
+      name: 'Staybridge Suites Cairo - Citystars',
+      image:
+        'https://www.fairmont.com/assets/0/104/2361/2366/4691/4692/1fc71b0d-63b1-4630-babd-5a54aa16d5b3.jpg',
+      location: 'https://maps.app.goo.gl/2ep3axMDoQwaegga7',
+      country_id: 11,
+    },
+    {
+      name: 'Holiday Inn & Suites Cairo Maadi',
+      image:
+        'https://digital.ihg.com/is/image/ihg/holiday-inn-hotel-and-suites-cairo-8897663006-4x3?wid=750&fit=constrain',
+      location: 'https://maps.app.goo.gl/NMAaTXwV7p3bier57',
+      country_id: 11,
+    },
+    {
+      name: 'Kempinski nile Cairo',
+      image:
+        'https://www.kayak.com/rimg/himg/48/a1/1d/ice-302919-105502000-870092.jpg?width=720&height=576&crop=true',
+      location: 'https://maps.app.goo.gl/Lp978htKBgqL1YZG7',
+      country_id: 11,
+    },
+    {
+      name: 'Fairmont Nile city Cairo',
+      image:
+        'https://www.fairmont.com/assets/0/104/2361/2366/4691/4692/1fc71b0d-63b1-4630-babd-5a54aa16d5b3.jpg',
+      location: 'https://maps.app.goo.gl/eJy8zarBPCSitdyh7',
+      country_id: 11,
+    },
+
+    {
+      name: 'Crowne Plaza West Cairo Arkan',
+      image:
+        'https://digital.ihg.com/is/image/ihg/crowne-plaza-cairo-7587750491-4x3?wid=750&fit=constrain',
+      location: 'https://maps.app.goo.gl/RX4Wr7R72LBB8wZh9',
+      country_id: 11,
+    },
+    {
+      name: ' Raffles Hotel',
+      image:
+        'https://m.ahstatic.com/is/image/accorhotels/aja_p_5484-68?wid=1920',
+      location: 'https://maps.app.goo.gl/bchFoax5sxxBQXnNA',
+      country_id: 44,
+    },
+    {
+      name: ' W Melbourne Hotel',
+      image:
+        'https://cf.bstatic.com/xdata/images/hotel/max1280x900/467646513.jpg?k=7cdfef934806535197f8832886d8ce054ac2bde3e6e9dd72ebf6dcb0a9096872&o=&hp=1',
+      location: 'https://maps.app.goo.gl/bchFoax5sxxBQXnNA',
+      country_id: 44,
     },
   ];
 }
