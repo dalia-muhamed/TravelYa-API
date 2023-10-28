65d29ab23ac787eab4d25d005dd401c3db41c111
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-10-28 02:11:28 +0200

    adding data

--- data.js
+++ data.js
@@ -2,44 +2,197 @@ export function getHotels() {
   return [
     {
       name: 'Staybridge Suites Cairo - Citystars',
-      image:
+      images: [
         'https://www.fairmont.com/assets/0/104/2361/2366/4691/4692/1fc71b0d-63b1-4630-babd-5a54aa16d5b3.jpg',
+        'https://lh3.googleusercontent.com/p/AF1QipOrWhVsEu3rDh0TW9Septfup65vUmmCcU46hbvV=w287-h192-n-k-rw-no-v1',
+        'https://lh3.googleusercontent.com/p/AF1QipPc1b3n6eIuGf2exPzZiysD0Rgs0w9szXXPUuOq=w287-h192-n-k-rw-no-v1',
+        'https://lh3.googleusercontent.com/gps-proxy/AFm_dcRJ2O4GwXAcOoV-K3cdpGE-a-t_TRUXSzGPoVVgrzxoBJ5tL3dSuXaEvkoMwZ2AOJsam-s7aye3rCov1VeivDyXPzJr0nng_zaMZXc2EYkWbV1eD-m0L3DckhgQoiREcaSjsi4pYng645cW8Y-HAcpzGvnAk4zfDMgFAZxt2F-7P2V2O9k_FslE=w287-h192-n-k-rw-no-v1',
+        'https://lh3.googleusercontent.com/p/AF1QipOSiC4wxcK4DIBJrcpBqayW0qBj5qDPM4DS0-PR=w287-h192-n-k-rw-no-v1',
+        'https://lh6.googleusercontent.com/proxy/vTfS50IVEiOEyLSlaHbrSkgOHg_UkSn-XTBhKFFfcjtX2q2XgKMx8G11wMqBJIcHd4m0JZPuJG2QWmivFrUhQUXHt_UJIn8PlBVAtV5WjSLUXIGxt8SLn2CmnN0LAOWbEO7yDn6KHKsXiyD__K_ImnYjcFPFaw=w253-h189-k-no',
+        'https://lh5.googleusercontent.com/p/AF1QipNUicBEMsRThlRp6FcQSWjcJp62jmf-biNa7rFS=w253-h189-k-no',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/46/2c/7f/dusit-thani-lakeview.jpg?w=1100&h=-1&s=1',
+      ],
+
       location: 'https://maps.app.goo.gl/2ep3axMDoQwaegga7',
       price: '150$',
       country_id: 11,
+      id: 20,
+    },
+    {
+      name: 'Marriott Mena House, Cairo',
+      images: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/ff/01/29/marriott-mena-house-cairo.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/ff/09/4d/deluxe-pyramid-view.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/1d/27/92/mena-house-hotel.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/70/18/05/moghul-room-serving-indian.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/15/d7/2d/spend-qauality-time-with.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/d4/dd/97/marriott-mena-house-swimming.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/b0/c1/fb/enjoy-sunny-days-at-one.jpg?w=900&h=-1&s=1',
+      ],
+
+      location: 'https://maps.app.goo.gl/kYiNtJKWBxJ3pVYZ8',
+      price: '167$',
+      country_id: 11,
+      id: 28,
+    },
+    {
+      name: 'Ramses Hilton',
+      images: [
+        'https://i.pinimg.com/564x/7a/a1/94/7aa19415266b8ad0ad570b7d2ecc3e8e.jpg',
+        'https://lh3.googleusercontent.com/p/AF1QipMUA3OoTbVB6_XxZO1WaEuC2Gof6T7rHI2cFgtv=w287-h192-n-k-rw-no-v1',
+        'https://lh5.googleusercontent.com/p/AF1QipNhk0KnAV6lc23CDBmq68pbBZkdAC50EY7KhjtK=w253-h168-k-no',
+        'https://lh5.googleusercontent.com/p/AF1QipOIK1aZmiBdiPQRcrDs09ZsfpS9mUaobj8iefz2=w253-h189-k-no',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/d4/87/ec/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://lh5.googleusercontent.com/p/AF1QipMUjLYKH93ChkP0-TKwoEbTk7Ng6cxIjCzc5vHI=w253-h168-k-no',
+        'https://lh5.googleusercontent.com/p/AF1QipOtPlv8bNdqw000g46fSJplNRkofHuUQEN0PMJU=w253-h168-k-no',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/80/c6/77/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+      ],
+
+      location: 'https://maps.app.goo.gl/QBGNXCoxDz1H2qt28',
+      price: '187$',
+      country_id: 11,
+      id: 29,
     },
     {
       name: 'Holiday Inn & Suites Cairo Maadi',
-      image:
+      images: [
         'https://digital.ihg.com/is/image/ihg/holiday-inn-hotel-and-suites-cairo-8897663006-4x3?wid=750&fit=constrain',
+        'https://lh5.googleusercontent.com/p/AF1QipOMZY4KdIi8HuEBTiAGIK-fs7nM1XWxkSVyEwfE=w253-h337-k-no',
+        'https://lh5.googleusercontent.com/p/AF1QipNtXdEUAjnwS0sN0HaSuITzUPIFwn0dFkFpP86e=w253-h168-k-no',
+        'https://lh5.googleusercontent.com/p/AF1QipOMZY4KdIi8HuEBTiAGIK-fs7nM1XWxkSVyEwfE=w253-h337-k-no',
+        'https://lh5.googleusercontent.com/p/AF1QipM2G9aT-Sz43L1RRj6ccUPsV4zRZ_PzoGgECWLj=w253-h168-k-no',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/46/2c/2f/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/05/66/dc/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/80/c5/ef/dusit-thani-lakeview.jpg?w=1100&h=-1&s=1',
+      ],
       location: 'https://maps.app.goo.gl/NMAaTXwV7p3bier57',
       price: '170$',
       country_id: 11,
+      id: 21,
     },
     {
       name: 'Kempinski nile Cairo',
-      image:
+      image: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/22/98/80/hotel-exterior-during.jpg?w=1200&h=-1&s=1',
+        'https://i.pinimg.com/564x/e4/dc/8c/e4dc8c7510ba4e1d201ad16eded671ab.jpg',
         'https://www.kayak.com/rimg/himg/48/a1/1d/ice-302919-105502000-870092.jpg?width=720&height=576&crop=true',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/22/b7/48/rooftop-pool-during-sunset.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/22/b8/bd/imperial-suite-jacuzzi.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/5e/c7/86/the-chocolate-lounge.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/22/93/65/chocolate-lounge.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/45/82/65/devarana-spa.jpg?w=1200&h=-1&s=1',
+      ],
+
       location: 'https://maps.app.goo.gl/Lp978htKBgqL1YZG7',
       price: '200$',
       country_id: 11,
+      id: 22,
     },
     {
       name: 'Fairmont Nile city Cairo',
-      image:
+      image: [
         'https://www.fairmont.com/assets/0/104/2361/2366/4691/4692/1fc71b0d-63b1-4630-babd-5a54aa16d5b3.jpg',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/5e/82/40/exterior-view.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/5e/82/ab/guest-room.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/00/e3/06/guest-room.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/00/e3/18/guest-room.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/00/e3/04/guest-room.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/5e/83/4d/recreational-facility.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/56/4d/44/pool--v5053901.jpg?w=1200&h=-1&s=1',
+      ],
+
       location: 'https://maps.app.goo.gl/eJy8zarBPCSitdyh7',
       price: '300$',
       country_id: 11,
+      id: 23,
     },
 
     {
-      name: 'Crowne Plaza West Cairo Arkan',
-      image:
-        'https://digital.ihg.com/is/image/ihg/crowne-plaza-cairo-7587750491-4x3?wid=750&fit=constrain',
+      name: 'Four Seasons Hotel Cairo at the First Residence',
+
+      images: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/ab/99/f7/a-hero-image.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/2e/8f/1a/view-of-the-pyramid-at.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/23/26/f1/e3/four-seasons-hotel-cairo.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/2e/8f/1b/spa.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/91/ae/2c/caption.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/a0/17/bc/aura-restaurant-day-shot.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/68/c0/df/deluxe-twin-room.jpg?w=1100&h=-1&s=1',
+      ],
+      location: 'https://maps.app.goo.gl/tPKW3wA1F6B54Kys6',
+      price: '147$',
+      country_id: 11,
+      id: 24,
+    },
+    {
+      name: 'Dust Thani LakeView Cairo',
+      images: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/80/8f/8f/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/59/c1/34/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/80/c6/dc/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/05/66/c2/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/80/c7/b5/dusit-thani-lakeview.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/80/90/f2/dusit-thani-lakeview.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/59/c4/07/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/84/51/ef/dusit-thani-lakeview.jpg?w=1200&h=-1&s=1',
+      ],
       location: 'https://maps.app.goo.gl/RX4Wr7R72LBB8wZh9',
       price: '147$',
       country_id: 11,
+      id: 25,
+    },
+    {
+      name: 'Sofitel Cairo Nile El Gezirah',
+      images: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/22/09/30/9f/sofitel-cairo-nile-el.jpg?w=1200&h=-1&s=1 ',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/ce/de/40/kebabgy-upper-terrace.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/f2/4c/7f/la-palmeraie-restaurant.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/6b/53/33/sofitel-cairo-el-gezirah.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/80/6e/4d/pool.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/80/6e/71/restaurant.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/80/6e/60/restaurant.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/8d/85/6b/restaurant.jpg?w=1100&h=-1&s=1',
+      ],
+      location: 'https://maps.app.goo.gl/Rfv4uSsv6dXZpxT77',
+      price: '192$',
+      country_id: 11,
+      id: 26,
+    },
+    {
+      name: 'Hyatt Regency Cairo West',
+      images: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/55/5d/c0/pool-view.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/62/f3/36/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/62/f3/1d/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/9c/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/8b/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/4f/hyatt-regency-cairo-west.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/5e/hyatt-regency-cairo-west.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/7c/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        '',
+      ],
+      location: 'https://maps.app.goo.gl/SHu8BpW2Nqu3DXQg8',
+      price: '167$',
+      country_id: 11,
+      id: 27,
+    },
+    {
+      name: 'Movenpick Hotel Cairo-Media City',
+      images: [
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/55/5d/c0/pool-view.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/62/f3/36/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/62/f3/1d/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/9c/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/8b/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/4f/hyatt-regency-cairo-west.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/5e/hyatt-regency-cairo-west.jpg?w=1100&h=-1&s=1',
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/64/98/7c/hyatt-regency-cairo-west.jpg?w=1200&h=-1&s=1',
+        '',
+      ],
+      location: 'https://maps.app.goo.gl/5SnAwasC43EU2hYr9',
+      price: '191$',
+      country_id: 11,
+      id: 30,
     },
     {
       name: ' Raffles Hotel',
@@ -48,6 +201,7 @@ export function getHotels() {
       location: 'https://maps.app.goo.gl/bchFoax5sxxBQXnNA',
       price: '147$',
       country_id: 44,
+      id: 48,
     },
     {
       name: ' W Melbourne Hotel',
@@ -91,38 +245,42 @@ export function getRestaurant() {
       image:
         'https://lh5.googleusercontent.com/p/AF1QipM6j2cjs5U7fK_EiQurPmnX0uqiPUNnUST4rS06=w260-h175-n-k-no',
       location: '54 Rd 9, Maadi, Cairo Governorate 4212011',
-        id: 11,
+      country_id: 11,
     },
     {
       name: 'Tree Lounge Maadi ',
       image:
         'https://lh3.googleusercontent.com/p/AF1QipMePG6E4vzRKTRkjDGZdsTZytbKK7YCnSILoAV4=s1360-w1360-h1020',
-      location:'Maadi Al Khabiri Ash Sharqeyah, Maadi, Cairo Governorate 11728',
-        id: 11,
+      location:
+        'Maadi Al Khabiri Ash Sharqeyah, Maadi, Cairo Governorate 11728',
+      country_id: 11,
     },
     {
       name: 'Griix',
-      image: 'https://lh3.googleusercontent.com/p/AF1QipMdCIJ14s2gA4Jf9zGzp7nsQFyQnX2JaxzixV-E=s680-w680-h510',
+      image:
+        'https://lh3.googleusercontent.com/p/AF1QipMdCIJ14s2gA4Jf9zGzp7nsQFyQnX2JaxzixV-E=s680-w680-h510',
       location: '38 street 231, degla, Maadi, Cairo Governorate',
-      id: 11,
+      country_id: 11,
     },
     {
       name: 'Lucca Italian Restaurant',
-      image: 'https://storage.kempinski.com/cdn-cgi/image/w=480,h=550,f=auto,g=auto,fit=cover/ki-cms-prod/images/1/6/2/5/95261-1-eng-GB/62213d1650f3-73658083_4K.jpg',
+      image:
+        'https://storage.kempinski.com/cdn-cgi/image/w=480,h=550,f=auto,g=auto,fit=cover/ki-cms-prod/images/1/6/2/5/95261-1-eng-GB/62213d1650f3-73658083_4K.jpg',
       location: 'Ring Rd, Second New Cairo, Cairo, Cairo Governorate 11477',
-      id: 11,
+      country_id: 11,
     },
     {
       name: 'The Grill',
       image: 'https://resizer.otstatic.com/v2/photos/xlarge/4/26670259.webp',
-      location: 'Semiramis InterContinental, Cornich, El Nile, Cairo Governorate 11511',
-      id: 11,
+      location:
+        'Semiramis InterContinental, Cornich, El Nile, Cairo Governorate 11511',
+      country_id: 11,
     },
     {
       name: 'QUAY',
       image:
         'https://www.australia.com/content/australia/en/things-to-do/food-and-drinks/best-restaurants-in-australia/jcr:content/mainParsys/imagewithcaption_1431434042/LargeImageTile/largeImageSrc.adapt.740.medium.jpg',
-      id: 44,
+      country_id: 44,
     },
   ];
 }
