6af05647bb7cbe2cbb67284473ded8306cfc4a3d
mina karam <minakaramkhalil.28@gmail.com>
2023-10-31 15:02:17 +0200

    push

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-import express from 'express';
-import { getCities, getHotels, getRestaurant, getThingsToDo } from './data.js';
+import express from "express";
+import { getCities, getHotels, getRestaurant, getThingsToDo } from "./data.js";
 const app = express();
-import cors from 'cors';
+import cors from "cors";
 app.use(cors());
-app.get('/hotels', (req, res) => {
+app.get("/hotels", (req, res) => {
   res.json({ hotels: getHotels() });
 });
-app.get('/cities', (req, res) => {
+app.get("/cities", (req, res) => {
   res.json({ cities: getCities() });
 });
-app.get('/restaurants', (req, res) => {
+app.get("/restaurants", (req, res) => {
   res.json({ restaurants: getRestaurant() });
 });
-app.get('/thingsToDo', (req, res) => {
+app.get("/thingsToDo", (req, res) => {
   res.json({ thingsToDo: getThingsToDo() });
 });
-app.get('/cities/:search', (req, res) => {
+app.get("/cities/:search", (req, res) => {
   const search = req.params.search;
   const cityName = req.query.cityName;
   const country = getCities().find(
-    city => city.name.toLocaleLowerCase() === cityName
+    (city) => city.name.toLocaleLowerCase() === cityName.toLocaleLowerCase()
   );
   const countryId = country.id;
   switch (search) {
-    case 'hotels':
+    case "hotels":
       const hotels = getHotels().filter(
-        hotel => hotel.country_id === countryId
+        (hotel) => hotel.country_id === countryId
       );
       return res.json({ hotels });
-      case 'thingsToDo':
+    case "thingsToDo":
       const thingsToDo = getThingToDo().filter(
-        thingsToDo => thingsToDo.country_id === countryId
+        (thingsToDo) => thingsToDo.country_id === countryId
       );
       return res.json({ thingsToDo });
-      case 'restaurants':
-        const restaurants = getRestaurant().filter(
-          restaurant => restaurant.country_id === countryId
-        );
+    case "restaurants":
+      const restaurants = getRestaurant().filter(
+        (restaurant) => restaurant.country_id === countryId
+      );
       return res.json({ restaurants });
-
   }
 });
 
