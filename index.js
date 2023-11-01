e2766cf103fcfb83828dfd4aef2b2ce996cf385a
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-11-02 00:03:36 +0200

    adding thingsToDo function

--- index.js
+++ index.js
@@ -1,81 +1,105 @@
-import express from "express";
-import { getCities, getHotels, getRestaurant, getThingsToDo } from "./data.js";
+import express, { json } from 'express';
+import { getCities, getHotels, getRestaurant, getThingsToDo } from './data.js';
 
 const app = express();
-import cors from "cors";
+import cors from 'cors';
 app.use(cors());
 
-app.get("/hotels", (req, res) => {
+app.get('/hotels', (req, res) => {
   res.json({ hotels: getHotels() });
 });
 
-app.get("/cities", (req, res) => {
+app.get('/cities', (req, res) => {
   res.json({ cities: getCities() });
 });
 
-app.get("/restaurants", (req, res) => {
+app.get('/restaurants', (req, res) => {
   res.json({ restaurants: getRestaurant() });
 });
 
-app.get("/thingsToDo", (req, res) => {
+app.get('/thingsToDos', (req, res) => {
   res.json({ thingsToDo: getThingsToDo() });
 });
 
-app.get("/cities/hotels", (req, res) => {
+app.get('/cities/hotels', (req, res) => {
   const hotelName = req.query.hotelName;
   const cityName = req.query.cityName;
   const cities = getCities();
   const hotels = getHotels();
 
-  const filteredHotels = hotels.filter((hotel) =>
-    hotel.name.toLowerCase().includes((hotelName || "").toLowerCase())
+  const filteredHotels = hotels.filter(hotel =>
+    hotel.name.toLowerCase().includes((hotelName || '').toLowerCase())
   );
 
-  const country = cities.find((city) => {
+  const country = cities.find(city => {
     if (city && city.name) {
-      return city.name.toLowerCase() === (cityName || "").toLowerCase();
+      return city.name.toLowerCase() === (cityName || '').toLowerCase();
     }
     return false;
   });
 
   if (country) {
-    const hotelsInCountry = hotels.filter((hotel) => hotel.country_id === country.id);
+    const hotelsInCountry = hotels.filter(
+      hotel => hotel.country_id === country.id
+    );
     return res.json({ hotels: hotelsInCountry });
   }
 
   res.json({ hotels: filteredHotels });
 });
 
-
-
-app.get("/cities/restaurants", (req, res) => {
+app.get('/cities/restaurants', (req, res) => {
   const restaurantName = req.query.restaurantName;
   const cityName = req.query.cityName;
   const cities = getCities();
   const restaurants = getRestaurant();
 
-  const filteredRestaurants = restaurants.filter((restaurant) =>
-  restaurant.name.toLowerCase().includes((restaurantName || "").toLowerCase())
+  const filteredRestaurants = restaurants.filter(restaurant =>
+    restaurant.name.toLowerCase().includes((restaurantName || '').toLowerCase())
   );
 
-  const country = cities.find((city) => {
+  const country = cities.find(city => {
     if (city && city.name) {
-      return city.name.toLowerCase() === (cityName || "").toLowerCase();
+      return city.name.toLowerCase() === (cityName || '').toLowerCase();
     }
     return false;
   });
 
   if (country) {
-    const restaurantsInCountry = restaurants.filter((restaurant) => restaurant.country_id === country.id);
+    const restaurantsInCountry = restaurants.filter(
+      restaurant => restaurant.country_id === country.id
+    );
     return res.json({ restaurants: restaurantsInCountry });
   }
 
   res.json({ restaurants: filteredRestaurants });
 });
 
+app.get('/cities/thingsToDos', (req, res) => {
+  const thingsToDoName = req.query.thingsToDoName;
+  const cityName = req.query.cityName;
+  const cities = getCities();
+  const thingsToDos = getThingsToDo();
 
+  const filteredToDos = thingsToDos.filter(todo =>
+    todo.name.toLowerCase().includes((thingsToDoName || '').toLowerCase())
+  );
 
+  const country = cities.find(city => {
+    if (city && city.name) {
+      return city.name.toLowerCase() === (cityName || '').toLowerCase();
+    }
+    return false;
+  });
 
+  if (country) {
+    const toDosInCountry = thingsToDos.filter(
+      todo => todo.country_id === country.id
+    );
+    return res.json({ todos: toDosInCountry });
+  }
 
+  res.json({ todos: filteredToDos });
+});
 
 app.listen(8080);
