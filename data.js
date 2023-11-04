18a9f6ac28dd9ca26fd1bd1e4bcc30acdc46ebfb
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-11-05 01:45:53 +0200

    update missing

--- data.js
+++ data.js
@@ -151,6 +151,7 @@ export function getHotels() {
         locationAddress:
           'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.9374844920058!2d31.22783437532024!3d30.03865131884551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145840cdb8f441cb%3A0x59cf96cf4cfe8978!2sKempinski%20Nile%20Hotel%20Garden%20City%20Cairo!5e0!3m2!1sen!2seg!4v1698461931751!5m2!1sen!2seg',
       },
+      phone: '+02 27 980 000',
       rating: 4.5,
       price: '200$',
       country_id: 11,
