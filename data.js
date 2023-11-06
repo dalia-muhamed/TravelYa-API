c3b7022109f992490ed8776f7c8e2e59391a86ae
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-11-06 03:02:06 +0200

    ..

--- data.js
+++ data.js
@@ -2595,7 +2595,7 @@ export function getThingsToDo() {
     {
       name: 'Colosseum',
       images: [
-        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/eb/91/d1/il-colosseo-di-roma-visto.jpg?w=1200&h=-1&s=1',
+        'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/d6/e9/c4.jpg',
         'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/f0/6b/18/gita-fuori-porta-avevo.jpg?w=1100&h=-1&s=1',
         'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/e3/16/2f/20181227-141917-largejpg.jpg?w=1200&h=-1&s=1',
         'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/cd/b1/b1/caption.jpg?w=1200&h=-1&s=1',
