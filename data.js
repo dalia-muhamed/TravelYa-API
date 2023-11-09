eefcd97e2841f0d7f73f0f8baf08e3303e1bc29f
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-11-09 05:07:17 +0200

    ..

--- data.js
+++ data.js
@@ -2140,7 +2140,9 @@ export function getRestaurant() {
       name: 'Zerofat',
       images: [
         'https://eatapp.co/abu-dhabi-restaurants/images/zerofat-abu-dhabi-the-view-tower-al-muroor-road-restaurant-1.jpg?height=500&width=850',
+        'https://media-cdn.tripadvisor.com/media/photo-s/1a/a2/2e/fd/protein-packed-shroom.jpg',
         'https://www.caterermiddleeast.com/2021/05/5vnxAO5f-ZEROFAT-ABU-DHABI-5.jpg',
+        'https://media-cdn.tripadvisor.com/media/photo-s/1c/9f/c5/ec/14-we-got-you-covered.jpg',
       ],
       location: {
         locationName: 'Al Wasl Road Wasl Vita Mall, Dubai United Arab Emirates',
